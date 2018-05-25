0c43ef295b50cc5d6568597ba1c324954f38570b
Nabihah <nabihah.student@gmail.com>
2018-05-25 12:43:33 +0800

    push

--- test.sql
+++ test.sql
@@ -1,3 +1,23 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 25, 2018 at 06:42 AM
+-- Server version: 10.1.29-MariaDB
+-- PHP Version: 7.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `test`
 --
@@ -55,7 +75,7 @@ CREATE TABLE `tbl_product` (
 INSERT INTO `tbl_product` (`id`, `name`, `image`, `price`) VALUES
 (1, 'Chicken chop', 'chicken chop.jpg', 10.90),
 (2, 'Lamb Chop', 'lamb chop.jpg', 12.90),
-(3, 'Fish ''N'' Chip', 'fishNchip.jpg', 11.90),
+(3, 'Fish \'N\' Chip', 'fishNchip.jpg', 11.90),
 (4, 'Nasi Kerabu', 'nasi kerabu.jpg', 6.00),
 (7, 'Roti Canai', 'roti canai.jpg', 1.20),
 (5, 'Chicken Rice', 'nasi ayam.jpg', 5.00),
@@ -100,6 +120,8 @@ ALTER TABLE `tbl_product`
 --
 ALTER TABLE `tbl_product`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
